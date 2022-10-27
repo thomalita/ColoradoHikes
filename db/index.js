8416b79b36462b1af965d5ba3156317d70de762f
Thomalita Burbank <thomalita37@gmail.com>
2022-10-27 07:58:34 -0600

    deployment to Heroku

--- db/index.js
+++ db/index.js
@@ -1,16 +1,17 @@
 const mongoose = require('mongoose')
+require('dotenv').config() // Add this line
 
-let MONGODB_URI = 'mongodb://127.0.0.1:27017/hikesDatabase'
+let dbUrl = process.env.NODE_ENV === 'production' ? process.env.MONGODB_URI : 'mongodb://127.0.0.1:27017/hikesDatabase'
 
 mongoose
-    .connect(MONGODB_URI)
-    .then(() => {
-        console.log('Successfully connected to MongoDB.')
-    })
-    .catch(e => {
-        console.error('Connection error', e.message)
-    })
-
+  .connect(dbUrl)
+  .then(() => {
+    console.log('Successfully connected to MongoDB!')
+  })
+  .catch((e) => {
+    console.error('Connection error', e.message)
+  })
+mongoose.set('debug', true)
 const db = mongoose.connection
 
 module.exports = db
